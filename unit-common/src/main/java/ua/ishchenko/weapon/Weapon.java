2e0afd6711862cdda17d8a4e500a440db5e08c58
Yaroslav-Ishchenko <jaros6522@gmail.com>
2014-11-23 21:06:27 +0200

    refactor of project's structure

---
+++ unit-common/src/main/java/ua/ishchenko/weapon/Weapon.java
@@ -0,0 +1,9 @@
+package ua.ishchenko.weapon;
+
+/**
+ * Created by Yaroslav on 11/22/2014.
+ */
+public interface Weapon {
+
+}
+
---
+++ unit-services/src/main/java/ua/units/services/impl/units/Grenader.java
@@ -0,0 +1,48 @@
+package ua.units.services.impl.units;
+
+import ua.ishchenko.unit.Unit;
+import ua.ishchenko.weapon.Weapon;
+
+import java.lang.Override;import java.lang.String;
+
+public class Grenader implements Unit {
+    private String name;
+    private Weapon weapon;
+
+    public Grenader() {
+        name = "Unknown";
+    }
+
+    public Grenader(String name) {
+        this.name = name;
+    }
+
+    public Grenader(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public Grenader(String name, Weapon weapon) {
+        this.name = name;
+        this.weapon = weapon;
+    }
+
+    @Override
+    public void atack() {
+
+    }
+
+    @Override
+    public void showTheWeapon() {
+
+    }
+
+    @Override
+    public void stopAtack() {
+
+    }
+
+    @Override
+    public void hitAnEnemy() {
+
+    }
+}

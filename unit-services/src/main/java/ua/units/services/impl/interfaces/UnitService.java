1153da985024158238256279103a036140c902ae
Yaroslav <jaros6522@gmail.com>
2014-11-23 21:08:19 +0200

    Create README.md

---
+++ unit-services/src/main/java/ua/units/services/impl/interfaces/UnitService.java
@@ -0,0 +1,12 @@
+package ua.units.services.impl.interfaces;
+
+import ua.ishchenko.services.UnitServiceCommonInterface;
+import ua.ishchenko.unit.Unit;
+
+/**
+ * Created by Yaroslav on 11/26/2014.
+ */
+public interface UnitService extends UnitServiceCommonInterface {
+
+
+}
